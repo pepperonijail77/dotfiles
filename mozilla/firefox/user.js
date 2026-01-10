d5af3c4248b51ebda78fcc4b5bca199f0857141d
pepperonijail77 <lucvanderwerf11@tutamail.com>
2026-01-10 14:40:10 +0000

    Update Firefox user.js
    Add additional lines for Papirus Firefox Icon theme

--- mozilla/firefox/user.js
+++ mozilla/firefox/user.js
@@ -95,3 +95,12 @@ user_pref("toolkit.telemetry.unifiedIsOptIn", false);
 user_pref("toolkit.telemetry.updatePing.enabled", false);
 user_pref("webgl.renderer-string-override", " ");
 user_pref("webgl.vendor-string-override", " ");
+
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Set UI density to normal
+user_pref("browser.uidensity", 0);
+
+// Enable SVG context-propertes
+user_pref("svg.context-properties.content.enabled", true);
