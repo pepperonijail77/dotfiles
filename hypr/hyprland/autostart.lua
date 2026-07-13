933fe03a8d51a71480519d31d9df4afee71ac986
pepperonijail77 <lucvanderwerf11@tutamail.com>
2026-07-13 15:37:36 +0100

    Switch to Swaybg and Swayidle

--- hypr/hyprland/autostart.lua
+++ hypr/hyprland/autostart.lua
@@ -5,13 +5,13 @@
 -- https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function ()
-    hl.exec_cmd("uwsm-app -- hyprpaper")
+    hl.exec_cmd("uwsm-app -- swaybg -i ~/.wallpapers/nord.jpg")
     hl.exec_cmd("uwsm-app -- waybar")
     hl.exec_cmd("hyprpm reload")
     hl.exec_cmd("wl-clip-persist --clipboard regular")
     hl.exec_cmd("uwsm-app -- swaync")
     hl.exec_cmd("uwsm-app -- swayosd-server")
-    hl.exec_cmd("uwsm-app -- hypridle")
+    hl.exec_cmd("uwsm-app -- swayidle")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
     hl.exec_cmd("wl-paste --watch clipvault store")
 end)
